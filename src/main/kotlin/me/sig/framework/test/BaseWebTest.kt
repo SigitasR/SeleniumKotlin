3363a492dcd888f86333a20f509c997fcabc39fb
Sigitas Radzevičius <sigitas.radzevicius@vinted.com>
2021-01-29 11:54:30 +0200

    Unused BeforeTest removed

--- src/main/kotlin/me/sig/framework/test/BaseWebTest.kt
+++ src/main/kotlin/me/sig/framework/test/BaseWebTest.kt
@@ -1,17 +1,9 @@
 package me.sig.framework.test
 
 import me.sig.framework.browser.BrowserFactory
-import org.openqa.selenium.WebDriver
 import org.testng.annotations.AfterMethod
-import org.testng.annotations.BeforeMethod
 
 open class BaseWebTest {
-    protected lateinit var driver: WebDriver
-
-    @BeforeMethod(alwaysRun = true)
-    fun before() {
-        this.driver = BrowserFactory.getDriverInstance()
-    }
 
     @AfterMethod(alwaysRun = true)
     fun tearDown() {
