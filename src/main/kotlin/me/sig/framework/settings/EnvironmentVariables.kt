bcc11b952f419785ae8757a55c5c0cccf9641ebc
Sigitas Radzevičius <sigitas.radzevicius@vinted.com>
2021-01-29 09:09:26 +0200

    Update to .gitignore

--- src/main/kotlin/me/sig/framework/settings/EnvironmentVariables.kt
+++ src/main/kotlin/me/sig/framework/settings/EnvironmentVariables.kt
@@ -1,5 +1,5 @@
 package me.sig.framework.settings
 
 val URL = System.getProperty("url", "https://www.swedbank.lt/private/credit/loans/home?language=ENG")
-val BROWSER = System.getProperty("browser", "Chrome")
+val BROWSER = System.getProperty("browser", "Firefox")
 val EXPLICIT_WAIT_TIMEOUT = System.getProperty("explicitWaitTimeout", "10").toLong()
